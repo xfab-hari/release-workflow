aa61018b07fae2a31c75ff8ac41dc255ddfba2f1
xfab-hari <hari@xfabric.ai>
2024-12-16 23:14:30 -0800

    update version

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ ver=${ver:1}
 new_ver=$(echo "$ver + 0.1" | bc)
 new_ver=v$new_ver
 
-git tag -a $new_ver -m "version v$new_ver"
+git tag -a releases/$new_ver -m "version v$new_ver"
 git push origin $new_ver
