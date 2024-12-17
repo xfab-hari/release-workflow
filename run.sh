602adcc86559e3c7eef6b0b1004d7b403685eb14
xfab-hari <hari@xfabric.ai>
2024-12-16 23:23:10 -0800

    update version

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@ git add .
 git commit -m "update version"
 git push origin main
 
-ver=$(git tag | tail -1)
-ver=${ver:1}
-new_ver=$(echo "$ver + 0.1" | bc)
-new_ver=v$new_ver
+# ver=$(git tag | tail -1)
+# ver=${ver:1}
+# new_ver=$(echo "$ver + 0.1" | bc)
+# new_ver=v$new_ver
 
-git tag -a releases/$new_ver -m "version v$new_ver"
-git push origin releases/$new_ver
+# git tag -a releases/$new_ver -m "version v$new_ver"
+# git push origin releases/$new_ver
